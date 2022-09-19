152580e5a27eaab674aeb5c57efc52560eaad3cb
ridemetothemoon12 <97011890+ridemetothemoon12@users.noreply.github.com>
2022-09-19 11:32:17 +0900

    Update index.js

--- naturedream/js/index.js
+++ naturedream/js/index.js
@@ -1,6 +1,21 @@
 $('document').ready(function() {
-    $(".m_btn").click(function() {
+    $(".btn_wrap").click(function() {
         $(".drop_menu").slideToggle();
         $(".btn_wrap").toggleClass("on");
+    })
+
+    $(".drop_menu .left ul li").click(function() {
+        $(".drop_menu .left ul li").removeClass("on");
+        $(this).addClass("on");
+    })
+
+    $(".content_3 .content .item").mouseenter(function() {
+        let i = $(this).index();
+        $(".content_3 .content .item .txt").eq(i).slideUp();
+        console.log("this"); 
+    })
+    $(".content_3 .content .item").mouseleave(function() {
+        $(".content_3 .content .item .txt").slideDown();  
+        console.log("that"); 
     })
 })
